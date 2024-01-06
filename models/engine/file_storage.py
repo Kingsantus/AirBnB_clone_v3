0ae4f1ef81a5d82a9c713d40dbc5aa429a60f0c9
kingsantus <kingsantusasogwa@gmail.com>
2024-01-06 07:03:29 -0800

    updated storage for db and file

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -58,6 +58,19 @@ class FileStorage:
         except:
             pass
 
+    def get(self, cls, id):
+        """retrieve one object"""
+        key = cls.__name__ + '.' + id
+
+        if key in self.__objects:
+            return self.__objects[key]
+        else:
+            return None
+
+    def count(self, cls=None):
+        """count the number of objects in storage """
+        return len(self.all(cls))
+
     def delete(self, obj=None):
         """delete obj from __objects if it’s inside"""
         if obj is not None:
